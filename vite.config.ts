7691a566c13efe3b933919dcb35e7423ee47cfb5
Patrizi5 <oneil521@hotmail.com>
2026-01-05 15:45:48 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src') // for Vite
     }
-  }
-  ,
+  },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
         '.js': 'jsx'
       }
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    alias: {
+      '@': path.resolve(__dirname, 'src') // ✅ make alias work in tests
     }
   }
 })
